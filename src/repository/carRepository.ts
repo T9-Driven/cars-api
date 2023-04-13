647d187a51ac222052f049a2ff5d59eecf8164ee
codethi <docencia.thiago@gmail.com>
2023-04-13 19:24:48 -0300

    feat: project

---
+++ src/repository/carRepository.ts
@@ -0,0 +1,46 @@
+import prisma from "../config/database.js";
+
+async function getCars() {
+  const data = await prisma.cars.findMany();
+  return data;
+}
+
+async function getCar(id: number) {
+  const data = await prisma.cars.findFirst({ where: { id } });
+  return data;
+}
+
+async function getCarWithLicensePlate(licensePlate: string) {
+  const data = await prisma.cars.findFirst({ where: { licensePlate } });
+  return data;
+}
+
+async function createCar(
+  model: string,
+  licensePlate: string,
+  year: number,
+  color: string
+) {
+  await prisma.cars.create({
+    data: {
+      color,
+      licensePlate,
+      model,
+      year,
+    },
+  });
+}
+
+async function deleteCar(id: number) {
+  await prisma.cars.delete({ where: { id } });
+}
+
+const carRepository = {
+  getCar,
+  getCarWithLicensePlate,
+  getCars,
+  createCar,
+  deleteCar,
+};
+
+export default carRepository;
