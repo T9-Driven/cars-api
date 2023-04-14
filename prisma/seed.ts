40f1e35af1585e4d9e197fe2a453cf32ad81f51f
codethi <docencia.thiago@gmail.com>
2023-04-14 19:31:04 -0300

    feat: add create categories

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,9 +1,16 @@
 import prisma from "../src/config/database.js";
 
 async function main() {
+  await prisma.categories.create({ data: { name: "SUV" } });
+  
   await prisma.cars.createMany({
     data: [
-      { model: "HB20", licensePlate: "ABC9094", year: 2001, color: "AZUL" },
+      {
+        model: "HB20",
+        licensePlate: "ABC9094",
+        year: 2001,
+        color: "AZUL",
+      },
       { model: "SONATA", licensePlate: "EZD3455", year: 2010, color: "PRETO" },
       { model: "X1", licensePlate: "FBI2276", year: 2018, color: "BRANCO" },
       { model: "CIVIC", licensePlate: "LPA3010", year: 2021, color: "CINZA" },
